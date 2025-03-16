03e25caff9cdc3b36fe0c473ac5a7b17fa0213d5
efficientlogix1 <efficientlogix1@gmail.com>
2025-03-16 21:02:03 +0500

    unit test added

---
+++ OrderProcessingApplication.Test/OrderServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingApplication.Test
+{
+    public class OrderServiceTest
+    {
+        [Fact]
+        public void GetOrder_ShouldReturnOK()
+        {
+            // Arrange
+            var orderService = new OrderService();
+
+            // Act
+            var result = orderService.GetOrder();
+
+            // Assert
+            Assert.Equal(100, result);
+        }
+    }
+}
